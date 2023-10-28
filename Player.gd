016cc5d789c343e1a466d7b0568c3010c7696751
Penta <142025868+oPenta@users.noreply.github.com>
2023-10-28 17:35:04 -0300

    Player.gd

--- Player.gd
+++ Player.gd
@@ -1,18 +1,19 @@
 extends CharacterBody2D
 
+
 @export var speed : float = 100
 @export var jump_force : float = 150
 
 @onready var animated_sprite : AnimatedSprite2D = $AnimatedSprite2D
 
-var grav = ProjectSettings.get_setting("physics/2d/default_gravity")
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 var animated_locked : bool = false
 var direction : Vector2 = Vector2.ZERO
 var in_air : bool = false
 
 func _physics_process(delta):
 	if !is_on_floor():
-		velocity.y += grav
+		velocity.y += gravity
 		in_air = true
 	
 	if in_air:
@@ -25,9 +26,8 @@ func _physics_process(delta):
 			jump()
 		
 	direction = Input.get_vector("Esquerda","Direita","Up", "s")
-	if direction.x != 0 && animated_sprite.animation != "jump_fall":
+	if direction:
 		velocity.x = direction.x * speed
-		
 	else:
 		velocity.x = move_toward(velocity.x, 0, speed)
 		
@@ -37,10 +37,13 @@ func _physics_process(delta):
 	
 func new_animation():
 	if !animated_locked:
-		if direction.x != 0:
-			animated_sprite.play("run")
+		if !is_on_floor():
+			animated_sprite.play("jump_fall")
 		else:
-			animated_sprite.play("idle")
+			if direction.x != 0:
+					animated_sprite.play("run")
+			else:
+					animated_sprite.play("idle")
 
 func direction_see():
 	if direction.x > 0:
@@ -61,3 +64,4 @@ func pousar():
 func _on_animated_sprite_2d_animation_finished():
 	if(animated_sprite.animation == "jump_fall"):
 		animated_locked = false
+
