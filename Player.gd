ebb9deb8d844dc69e71c999bc763d3db3fe00e8a
Penta <142025868+oPenta@users.noreply.github.com>
2023-10-28 16:57:55 -0300

    Add files via upload

---
+++ Player.gd
@@ -0,0 +1,63 @@
+extends CharacterBody2D
+
+@export var speed : float = 100
+@export var jump_force : float = 150
+
+@onready var animated_sprite : AnimatedSprite2D = $AnimatedSprite2D
+
+var grav = ProjectSettings.get_setting("physics/2d/default_gravity")
+var animated_locked : bool = false
+var direction : Vector2 = Vector2.ZERO
+var in_air : bool = false
+
+func _physics_process(delta):
+	if !is_on_floor():
+		velocity.y += grav
+		in_air = true
+	
+	if in_air:
+		pousar()
+		
+	in_air = false
+		
+	if Input.is_action_pressed("Up"):
+		if is_on_floor():
+			jump()
+		
+	direction = Input.get_vector("Esquerda","Direita","Up", "s")
+	if direction.x != 0 && animated_sprite.animation != "jump_fall":
+		velocity.x = direction.x * speed
+		
+	else:
+		velocity.x = move_toward(velocity.x, 0, speed)
+		
+	move_and_slide()
+	new_animation()
+	direction_see()
+	
+func new_animation():
+	if !animated_locked:
+		if direction.x != 0:
+			animated_sprite.play("run")
+		else:
+			animated_sprite.play("idle")
+
+func direction_see():
+	if direction.x > 0:
+		animated_sprite.flip_h = false
+	elif direction.x < 0:
+		animated_sprite.flip_h = true
+		
+func jump():
+	velocity.y -= jump_force
+	animated_sprite.play("jump")
+	animated_locked = true
+	
+func pousar():
+	animated_sprite.play("jump_fall")
+	animated_locked = true
+	
+
+func _on_animated_sprite_2d_animation_finished():
+	if(animated_sprite.animation == "jump_fall"):
+		animated_locked = false
