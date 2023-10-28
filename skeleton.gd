152e0185718d29682b590da5630d28a89d1f8576
Penta <142025868+oPenta@users.noreply.github.com>
2023-10-28 19:41:20 -0300

    skeleton.gd

--- skeleton.gd
+++ skeleton.gd
@@ -1,2 +1,15 @@
 extends CharacterBody2D
 
+@export var speed : float = 100
+@export var jump_force : float = 150
+
+@onready var animated_sprite : AnimatedSprite2D = $AnimatedSprite2D
+
+var gravity = 8
+
+func _physics_process(_delta):
+	if !is_on_floor():
+		velocity.y += gravity
+
+
+	move_and_slide()
