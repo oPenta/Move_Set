dbbeb7ac80ee639b4dbe39c299b2767df15a4f8c
Penta <142025868+oPenta@users.noreply.github.com>
2023-10-30 17:18:53 -0300

    skeleton.gd

--- skeleton.gd
+++ skeleton.gd
@@ -1,15 +1,62 @@
 extends CharacterBody2D
 
-@export var speed : float = 100
-@export var jump_force : float = 150
+enum StateMachine {idle, run, atack}
 
-@onready var animated_sprite : AnimatedSprite2D = $AnimatedSprite2D
+const speed := 60
+const dist_follow := 250
+const dist_atack := 35
 
-var gravity = 8
+var distance := 0.0
+var strong := 10
+var health := 3
+var animation := ""
+var state := StateMachine.idle
+var direction := 0
+var death := false
 
-func _physics_process(_delta):
-	if !is_on_floor():
-		velocity.y += gravity
+@onready var animation_player = $AnimationPlayer
+@onready var player = owner.get_node("Player")
+
+func _physics_process(delta: float) -> void:
+	distance = global_position.distance_to(player.global_position)
+	
+	match state:
+		StateMachine.idle:
+			_set_animation("idle")
+			if distance <= dist_follow:
+				_enter_state(StateMachine.run)
+				
+		StateMachine.run:
+			_set_animation("run")
+			_flip()
+			velocity.x = direction * speed
+			move_and_slide()
+			
+			if distance >= dist_follow:
+				_enter_state(StateMachine.idle)
+				
+			if distance <= dist_atack:
+				_enter_state(StateMachine.atack)
+			
+		StateMachine.atack:
+			_set_animation("atack")
+			await animation_player.animation_finished
+			_enter_state(StateMachine.idle)
 
 
-	move_and_slide()
+func _enter_state(new_state: StateMachine) -> void:
+	if state != new_state:
+		state = new_state
+
+func _set_animation(anim: String) -> void:
+	if animation != anim:
+		animation = anim
+		animation_player.play(animation)
+		
+func _flip() -> void:
+	if global_position.x < player.global_position.x:
+		$Sprite2D.flip_h = false
+		direction = 1
+	elif global_position.x > player.global_position.x:
+		$Sprite2D.flip_h = true
+		direction = -1
