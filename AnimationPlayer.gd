de77aad671d5393ed8ae73da2cb5d54c88deb77f
Penta <142025868+oPenta@users.noreply.github.com>
2023-11-04 19:28:42 -0300

    AnimationPlayer.gd

--- AnimationPlayer.gd
+++ AnimationPlayer.gd
@@ -1,49 +1,61 @@
 extends CharacterBody2D
 
-enum StateMachine{idle, run, atack1}
+enum StateMachine{idle, run, atack1, atack2}
 
-const speed := 100
-const jump_force := 150
-const gravity = 7.5
+var speed := 120
+var gravity = 8
 
+var death := false
 var strong := 30
 var health := 10
 var animation := ""
 var state := StateMachine.idle
-var direction : Vector2 = Vector2.ZERO
-var death := false
+var direction := 0.0
+var boost := direction * 50
+
 
 @onready var animation_player = $AnimationPlayer
 
-func _physics_process(_delta : float) -> void:
-	direction = Input.get_vector("Esquerda","Direita", "Up", "s")
+@export var timer : Timer = null
+
+func _ready():
+	pass
 	
+func _physics_process(_delta) -> void:
+	direction = Input.get_axis("Esquerda","Direita")
+	@warning_ignore("narrowing_conversion")
+	boost = direction * 50
+	
+	if !is_on_floor():
+		velocity.y += gravity
+		move_and_slide()
+		
 	match state:
 		StateMachine.idle:
 			_set_animation("idle")
-			if direction.x != 0:
+			if direction != 0:
 				_enter_state(StateMachine.run)
-			
+			if Input.is_action_pressed("Atack"):
+				_enter_state(StateMachine.atack1)
+		
 		StateMachine.run:
 			_set_animation("run")
 			_flip()
-			velocity.x = direction.x * speed
-			if direction.x == 0:
+			velocity.x = direction * speed
+			if Input.is_action_pressed("Cima"):
+				velocity.x = direction * speed + boost
+			if direction == 0:
 				_enter_state(StateMachine.idle)
-		
 			move_and_slide()
 			
 		StateMachine.atack1:
+			_set_animation("atack1")
+			await animation_player.animation_finished
+			_enter_state(StateMachine.idle)
+			
+		StateMachine.atack2: 
 			pass
-		
-	if !is_on_floor():
-		velocity.y += gravity
-		
-	if Input.is_action_pressed("Up") and is_on_floor():
-		jump()
-		
-		
-	
+			
 func _enter_state(new_state: StateMachine) -> void:
 	if state != new_state:
 		state = new_state
@@ -53,11 +65,8 @@ func _set_animation(anim: String) -> void:
 		animation = anim
 		animation_player.play(animation)
 		
-func jump() -> void:
-	velocity.y -= jump_force
-	
 func _flip() -> void:
-	if direction.x > 0:
+	if direction > 0:
 		$Sprite2D.flip_h = false
-	elif direction.x < 0:
+	elif direction < 0:
 		$Sprite2D.flip_h = true
